e4937451e0a9360b2972b62a3d5411cbc6231302
unknown <you@example.com>
2022-03-29 14:56:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//if statement
+let today =  new Date();
+
+if(today.getHours<10){
+    console.log("Jóreggeét");
+}else if(today.getHours<20){
+    console.log("Jónapot");
+}else{
+    console.log("Jóestét");
+}
+
+
+//create a for loop
+for (let i = 1; i < 3; i++) {
+    console.log(i)
+}
+
+
+//Listában való ismétlő ciklus futtatás
+let myList = ["item1", "item2", "item3"];
+for(let i = 0; i < myList.length; i++) {
+    console.log(myList[i]);
+}
+
+myList.forEach(myFunction);
+
+
+function myFunction(item, index, array) {
+    console.log(item);
+    console.log(index);
+    console.log(array);
+}
