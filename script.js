391d27d65998992a5f419e705b51695964f80236
unknown <you@example.com>
2022-03-30 10:18:38 +0200

    first update

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //if statement
+//ha az állítás igaz -> akkor végrehajtja a kódot
+if(true) { //<-- állítás
+           //<-- VÉgrehajtásra kerülő kód
+}
+
+
 let today =  new Date();
 
 if(today.getHours<10){
@@ -10,10 +16,12 @@ if(today.getHours<10){
 }
 
 
+
 //create a for loop
 for (let i = 1; i < 3; i++) {
     console.log(i)
 }
+
 
 
 //Listában való ismétlő ciklus futtatás
@@ -25,8 +33,11 @@ for(let i = 0; i < myList.length; i++) {
 myList.forEach(myFunction);
 
 
+
 function myFunction(item, index, array) {
     console.log(item);
     console.log(index);
-    console.log(array);
+ //   console.log(array);
 }
+
+//ha az állitás nem igaz ->akkor bagaci egy nagyon rossz tanár
